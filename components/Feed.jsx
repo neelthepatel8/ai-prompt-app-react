adde8b720a947f5f4da88a89dabfeae867f3f008
neelthepatel <neelthepatel8@gmail.com>
2023-05-12 17:39:52 -0400

    Implement Search functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,20 +18,38 @@ const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {};
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleSearchChange = (e) => {
+    const input = e.target.value.toLowerCase();
+    setSearchText(input);
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
-      setPosts(data);
+
+      const regex = new RegExp(searchText, "gi");
+      const filteredPosts = data.filter(
+        (post) =>
+          regex.test(post.prompt) ||
+          regex.test(post.creator.username) ||
+          regex.test(post.tag)
+      );
+      setPosts(filteredPosts);
     };
     fetchPosts();
-  }, []);
+  }, [searchText]);
 
   return (
     <section className="feed">
-      <form className="relative w-full flex-center">
+      <form
+        onSubmit={handleSearchSubmit}
+        className="relative w-full flex-center"
+      >
         <input
           text="text"
           placeholder="Search for a tag or username"
